f761b26734c7ed1fa134799f5c45ae127ec85dc8
Ansh Varma <anshvarma83691@gmail.com>
2024-10-04 22:53:29 +0530

    NAv

--- src/components/element/navbar.js
+++ src/components/element/navbar.js
@@ -1,27 +1,49 @@
+'use client'
 import React from "react";
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import classNames from "classnames";
 import { CaretDownIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+import Image from "next/image";
 
+
+const handleSlider = () => {
+    if (isOpen !== undefined && setIsOpen !== undefined) {
+      setIsOpen(!isOpen);
+    }
+  };
 
 const NavigationMenuDemo = () => {
+    
     return (
-        <NavigationMenu.Root className="relative z-10 flex w-screen justify-center">
+
+        <section className="flex flex-row justify-between pt-7">
+        
+        <Link
+        href="/"
+        className="ml-24"
+        onClick={handleSlider}
+        passHref>
+        {/* <Image src={Logo} className="h-9 w-9" alt="logo" /> */}
+        <span className="text-xl inline-block font-bold">ORATORIS</span>
+      </Link>
+        <NavigationMenu.Root className="relative z-10 flex w-screen justify-center" >
             <NavigationMenu.List className="center m-0 flex list-none rounded-md bg-white p-1">
                 <NavigationMenu.Item>
-                    <NavigationMenu.Trigger className="group flex select-none items-center justify-between gap-0.5 rounded px-3 py-2 text-[15px] font-medium leading-none text-violet11 outline-none hover:bg-violet3 focus:shadow-[0_0_0_2px] focus:shadow-violet7">
+
+                        <NavigationMenu.Trigger className="group flex select-none items-center justify-between gap-0.5 rounded px-3 text-[15px] font-medium leading-none">
                         Learn{" "}
                         <CaretDownIcon
-                            className="relative top-px text-violet10 transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180"
+                            className="relative transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180"
                             aria-hidden
                         />
                     </NavigationMenu.Trigger>
-                    <NavigationMenu.Content className="absolute left-0 top-0 w-full data-[motion=from-end]:animate-enterFromRight data-[motion=from-start]:animate-enterFromLeft data-[motion=to-end]:animate-exitToRight data-[motion=to-start]:animate-exitToLeft sm:w-auto">
+                    <NavigationMenu.Content className="absolute left-0 top-0 w-full sm:w-auto outline bg-slate-400">
                         <ul className="one m-0 grid list-none gap-x-2.5 p-[22px] sm:w-[500px] sm:grid-cols-[0.75fr_1fr]">
                             <li className="row-span-3 grid">
                                 <NavigationMenu.Link asChild>
                                     <a
-                                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-purple9 to-indigo9 p-[25px] no-underline outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet7"
+                                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-purple9 to-indigo9 p-[25px] no-underline  "
                                         href="/"
                                     >
                                         <svg
@@ -44,7 +66,7 @@ const NavigationMenuDemo = () => {
                                     </a>
                                 </NavigationMenu.Link>
                             </li>
-                            {/* 
+                            
 							<ListItem href="https://stitches.dev/" title="Stitches">
 								CSS-in-JS with best-in-class developer experience.
 							</ListItem>
@@ -53,70 +75,72 @@ const NavigationMenuDemo = () => {
 							</ListItem>
 							<ListItem href="https://icons.radix-ui.com/" title="Icons">
 								A crisp set of 15x15 icons, balanced and consistent.
-							</ListItem> */}
+							</ListItem>
                         </ul>
                     </NavigationMenu.Content>
                 </NavigationMenu.Item>
 
+
+{/* new */}
                 <NavigationMenu.Item>
-                    <NavigationMenu.Trigger className="group flex select-none items-center justify-between gap-0.5 rounded px-3 py-2 text-[15px] font-medium leading-none text-violet11 outline-none hover:bg-violet3 focus:shadow-[0_0_0_2px] focus:shadow-violet7">
+                    <NavigationMenu.Trigger className="group flex select-none items-center justify-between gap-0.5 rounded px-3 text-[15px] font-medium leading-none">
                         Overview{" "}
                         <CaretDownIcon
-                            className="relative top-px text-violet10 transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180"
+                            className="relative top-px  transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180"
                             aria-hidden
                         />
                     </NavigationMenu.Trigger>
-                    <NavigationMenu.Content className="absolute left-0 top-0 w-full sm:w-auto">
+                    <NavigationMenu.Content className="absolute left-0 top-0 w-full sm:w-auto bg-slate-400">
                         <ul className="m-0 grid list-none gap-x-2.5 p-[22px] sm:w-[600px] sm:grid-flow-col sm:grid-rows-3">
                             <ListItem
                                 title="Introduction"
                                 href="/primitives/docs/overview/introduction"
                             >
-                                Build high-quality, accessible design systems and web apps.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                             </ListItem>
                             <ListItem
                                 title="Getting started"
                                 href="/primitives/docs/overview/getting-started"
                             >
-                                A quick tutorial to get you up and running with Radix
-                                Primitives.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                             </ListItem>
                             <ListItem title="Styling" href="/primitives/docs/guides/styling">
-                                Unstyled and compatible with any styling solution.
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                             </ListItem>
                             <ListItem
                                 title="Animation"
                                 href="/primitives/docs/guides/animation"
                             >
-                                Use CSS keyframes or any animation library of your choice.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                             </ListItem>
                             <ListItem
                                 title="Accessibility"
                                 href="/primitives/docs/overview/accessibility"
                             >
-                                Tested in a range of browsers and assistive technologies.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                             </ListItem>
                             <ListItem
                                 title="Releases"
                                 href="/primitives/docs/overview/releases"
                             >
-                                Radix Primitives releases and their changelogs.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                             </ListItem>
                         </ul>
                     </NavigationMenu.Content>
                 </NavigationMenu.Item>
+{/* new */}
 
                 <NavigationMenu.Item>
                     <NavigationMenu.Link
-                        className="block select-none rounded px-3 py-2 text-[15px] font-medium leading-none text-violet11 no-underline outline-none hover:bg-violet3 focus:shadow-[0_0_0_2px] focus:shadow-violet7"
-                        href="https://github.com/radix-ui"
+                        className="block select-none rounded px-3 text-[15px] font-medium leading-none"
+                        href="https://github.com/anshhvarma/ORATORIS"
                     >
                         Github
                     </NavigationMenu.Link>
                 </NavigationMenu.Item>
 
                 <NavigationMenu.Indicator className="top-full z-10 flex h-2.5 items-end justify-center overflow-hidden transition-[width,transform_250ms_ease] data-[state=hidden]:animate-fadeOut data-[state=visible]:animate-fadeIn">
-                    <div className="relative top-[70%] size-2.5 rotate-45 rounded-tl-sm bg-white" />
+                    <div className="relative top-[70%] size-2.5 rotate-45 rounded-tl-sm bg-black" />
                 </NavigationMenu.Indicator>
             </NavigationMenu.List>
 
@@ -124,6 +148,10 @@ const NavigationMenuDemo = () => {
                 <NavigationMenu.Viewport className="relative mt-2.5 h-[var(--radix-navigation-menu-viewport-height)] w-full origin-[top_center] overflow-hidden rounded-md bg-white transition-[width,_height] duration-300 data-[state=closed]:animate-scaleOut data-[state=open]:animate-scaleIn sm:w-[var(--radix-navigation-menu-viewport-width)]" />
             </div>
         </NavigationMenu.Root>
+        <div className="mr-24 text-xl"> 
+            Search 
+        </div>
+        </section>
     );
 };
 
@@ -146,7 +174,9 @@ const ListItem = React.forwardRef(
                 </a>
             </NavigationMenu.Link>
         </li>
-    ),
+    )
 );
+
+ListItem.displayName = "ListItem";
 
 export default NavigationMenuDemo;
